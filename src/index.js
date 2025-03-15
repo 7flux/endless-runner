316bc181cedb45c1f50430404fb4171bbe0b3687
Roman Zvonyk <jagerluck@gmail.com>
2025-03-15 11:13:27 +0200

    menu: remove vanilla js part

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-import { createMenu } from '@/components/menu/createMenu';
+import Phaser from 'phaser'
+import { MenuScene } from '@/MenuScene';
+import { GameScene } from '@/GameScene';
 
-// import PlayScene from './PlayScene';
-// import PreloadScene from './PreloadScene';
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: [MenuScene, GameScene],
+  
+  // pixelArt: true,
+  // transparent: true,
+  // physics: {
+  //   default: 'arcade',
+  //   arcade: {
+  //     debug: false
+  //   }
+  // },
+  // scene: [PreloadScene, PlayScene]
+};
 
-createMenu();
+new Phaser.Game(config);
 
-// new Phaser.Game(config);
