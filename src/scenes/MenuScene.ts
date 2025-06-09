37d8d193a74d58c5f55446aa8cb007912bbe603e
Roman Zvonyk <jagerluck@gmail.com>
2025-06-09 20:24:55 +0300

    hoverable class

--- src/scenes/MenuScene.ts
+++ src/scenes/MenuScene.ts
@@ -1,3 +1,6 @@
+import { Actionable } from "@/utils/Hoverable";
+
+
 export class MenuScene extends Phaser.Scene {
   constructor() {
     super('MenuScene');
@@ -10,14 +13,6 @@ export class MenuScene extends Phaser.Scene {
   create() {
     let startButton = this.add.text(250, 250, 'Start Game', { fontSize: '32px', fill: '#fff' })
       .setInteractive()
-      .on('pointerover', () => {
-        startButton.setColor('#dddddd')
-        this.input.setDefaultCursor('pointer');
-      })
-      .on('pointerout', () => {
-        startButton.setColor('#fff')
-        this.input.setDefaultCursor('default');
-      })
       .on('pointerdown', () => {
         startButton.setColor('#fff')
         this.scene.start('GameScene')
@@ -25,18 +20,12 @@ export class MenuScene extends Phaser.Scene {
     
     let inventoryDebug = this.add.text(250, 200, 'Debug inventory', { fontSize: '32px', fill: '#fff' })
       .setInteractive()
-      .on('pointerover', () => {
-        inventoryDebug.setColor('#dddddd')
-        this.input.setDefaultCursor('pointer');
-      })
-      .on('pointerout', () => {
-        inventoryDebug.setColor('#fff')
-        this.input.setDefaultCursor('default');
-      })
       .on('pointerdown', () => {
         inventoryDebug.setColor('#fff')
         this.scene.start('MainScene')
       });
+    
+    Actionable.setHoverable([inventoryDebug, startButton], this.input);
   }
 
   showOptions() {
--- src/utils/Hoverable.ts
+++ src/utils/Hoverable.ts
@@ -1,5 +1,18 @@
-export class Hoverable {
-  constructor(object) {
-    
+export class Actionable {
+  static setHoverable(objects, input, config = { color: {
+    pointerover: '#dddddd',
+    pointerout: '#fff',
+  } }) {
+    objects.forEach((object) => {
+      object
+        .on('pointerover', () => {
+          input.setDefaultCursor('pointer');
+          object.setColor(config.color.pointerover);
+        })
+        .on('pointerout', () => {
+          input.setDefaultCursor('default');
+          object.setColor(config.color.pointerout);
+        })
+    })
   }
 }
